c2e8d87f42a72ab660e58e30e66bf4f7fe2a6c49
carlos <carlos.20499@gmail.com>
2020-11-22 18:29:29 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express()
-
+app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/', (req, res)=>{
